a8ee9abdd26eb343c3f9aab1abde636cd3b11134
khorh <helen.khor@googlemail.com>
2021-09-01 14:14:07 +0100

    added babel config file

--- main-functions.test.js
+++ main-functions.test.js
@@ -1,7 +1,7 @@
 import { calculateProbabilityOfPeter } from "./main-functions";
 
 describe ("probability tests", () => {
-    test("probability of Peter should return a number", () => {
+    test("probability of Peter should return a number between 0 and 100", () => {
         expect(calculateProbabilityOfPeter(1, 10)).toBe(10);
     })
 })
