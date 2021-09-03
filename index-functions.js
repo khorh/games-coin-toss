6dccf24ed091f09a69b17ba69c39bf1de38032d9
khorh <helen.khor@googlemail.com>
2021-09-03 08:22:41 +0100

    updated JS docu blocking to functions

--- index-functions.js
+++ index-functions.js
@@ -1,7 +1,8 @@
 /**
-* HEADER CONTAINER
-* Coin toss function to hide the header container and display the main container to start the coin toss
-*/
+ * HEADER CONTAINER
+ * Coin toss function to hide the header container and display the main container to start the coin toss
+ * @returns {string} hides the header container and shows the main container
+ */
 const startToss = () => {
     const headerContainerQuery = document.querySelector('#headerContainer');
     const mainContainerQuery = document.querySelector('#mainContainer');
@@ -10,11 +11,11 @@ const startToss = () => {
 };
 
 /**
-* MAIN CONTAINER
-* Coin toss function to display the image based on the randomise number of 1 and 2
-* @param {number} randomToss which is the random number of either 1 or 2
-* @returns {string} displays the image of a Peter or Benjamin 50p coin
-*/
+ * MAIN CONTAINER
+ * Coin toss function to display the image based on the randomise number of 1 and 2
+ * @param {number} randomToss which is the random number of either 1 or 2
+ * @returns {string} displays the image of a Peter or Benjamin 50p coin with the alt tag
+ */
 const displayToss = (randomToss) => {
     const tossImageQuery = document.querySelector('#tossImage');
     if (randomToss === 1) {
@@ -27,11 +28,11 @@ const displayToss = (randomToss) => {
 };
 
 /**
-* FOOTER CONTAINER
-* Coin toss function to add and display the counts of Peter and Benjamin
-* @param {number} randomToss which is the random number of either 1 or 2
-* @returns {string} displays the counts for Peter and Benjamin
-*/
+ * FOOTER CONTAINER
+ * Coin toss function to add and display the counts of Peter and Benjamin
+ * @param {number} randomToss which is the random number of either 1 or 2
+ * @returns {string} displays the counts for Peter and Benjamin
+ */
 const displayCount = (randomToss) => {
     const totalPeterQuery = document.querySelector('#totalPeter');
     const totalBenjaminQuery = document.querySelector('#totalBenjamin');
@@ -47,6 +48,8 @@ const displayCount = (randomToss) => {
 /**
  * FOOTER CONTAINER
  * Coin toss function to display the probability of Peter
+ * @param {number} totalTosses which is the total number of tosses
+ * @return {number} percentagePeter which is the probability of getting Peter as an integer
  */
 const calculateProbabilityOfPeter = (totalTosses) => {
     const calculatePercentagePeter = numberOfPeter/totalTosses*100;
@@ -57,6 +60,8 @@ const calculateProbabilityOfPeter = (totalTosses) => {
 /**
  * FOOTER CONTAINER
  * Coin toss function to display the probability of Benjamin
+ * @param {number} totalTosses which is the total number of tosses
+ * @return {number} percentagePeter which is the probability of getting Peter as an integer
  */
 const calculateProbabilityOfBenjamin = (totalTosses) => {
     const calculatePercentageBenjamin = numberOfBenjamin/totalTosses*100;
@@ -65,9 +70,11 @@ const calculateProbabilityOfBenjamin = (totalTosses) => {
 };
 
 /**
-* FOOTER CONTAINER
-* Coin toss function to display the probability of Peter and Benjamin
-*/
+ * FOOTER CONTAINER
+ * Coin toss function to display the probability of Peter and Benjamin
+ * @param {number} totalTosses which is the total number of tosses
+ * @return {string} probabilities of Peter and Benjamin
+ */
 const displayProbabilities = (totalTosses) => {
     const percentagePeterQuery = document.querySelector('#percentagePeter');
     const percentageBenjaminQuery = document.querySelector('#percentageBenjamin');
@@ -76,20 +83,21 @@ const displayProbabilities = (totalTosses) => {
 };
 
 /**
-* FOOTER CONTAINER
-* Coin toss function to display every click of the Toss! button
-* @param {number} totalTosses which is the total number of tosses
-* @returns {string} displays the total number of tosses
-*/
+ * FOOTER CONTAINER
+ * Coin toss function to display every click of the Toss! button
+ * @param {number} totalTosses which is the total number of tosses
+ * @returns {string} displays the total number of tosses
+ */
 const displayTotalTosses = (totalTosses) => {
     const totalTossesQuery = document.querySelector('#totalTosses');
     totalTossesQuery.innerText = totalTosses;
 };
 
 /**
-* FOOTER CONTAINER
-* Coin toss function to hide the header container and display the main container
-*/
+ * FOOTER CONTAINER
+ * Coin toss function to hide the header container and display the main container
+ * @returns {string} hides the main container and shows the footer container
+ */
 const endToss = () => {
     const mainContainerQuery = document.querySelector('#mainContainer');
     const footerContainerQuery = document.querySelector('#footerContainer');
